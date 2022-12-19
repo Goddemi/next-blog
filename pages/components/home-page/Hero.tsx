022e8d70071fa1ed4893bee626e4098488b5f979
goddemi <goddemi0124@gmail.com>
2022-12-19 18:21:29 +0900

    Add : Head component

---
+++ pages/components/home-page/Hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="flex justify-center">
+        <Image
+          src="/image/site/universe.jpg"
+          alt="space"
+          width={1000}
+          height={300}
+        />
+      </div>
+      <h1 className="text-center text-4xl py-20">
+        This is planet shopping mall
+      </h1>
+      <p className="text-center text-2xl">planet shopping mall for explorer</p>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import Hero from "./components/home-page/Hero";
+import FeaturedProduct from "./components/home-page/FeaturedProduct";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      {/* <Nav /> */}
-
+      <Hero />
+      <FeaturedProduct />
       <div className="text-3xl font-bold underline">hi</div>
     </>
   );
