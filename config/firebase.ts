496378b9b6b863e2c242b9c93bb388f301a58c56
goddemi <goddemi0124@gmail.com>
2023-01-01 22:47:53 +0900

    Modify : env파일 수정

--- config/firebase.ts
+++
@@ -1,2 +0,0 @@
-// export const firebaseTitle = process.env.firebase_title;
-// export const firebaseId = process.env.firebase_id;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
+// export const firebaseTitle = process.env.firebase_title;
+// export const firebaseId = process.env.firebase_id; --> 별도의 config파일안에 넣고
+
 const nextConfig = (phase) => {
   // if (phase === PHASE_DEVELOPMENT_SERVER) {
   //   return {
