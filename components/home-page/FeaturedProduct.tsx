a37cce1345acc862ecf4e4ee94e7737ae3fbe983
goddemi <goddemi0124@gmail.com>
2023-01-02 11:40:14 +0900

    Modify : css detail

--- components/home-page/FeaturedProduct.tsx
+++ components/home-page/FeaturedProduct.tsx
@@ -7,7 +7,7 @@ const FeaturedProduct = ({ products }: { products: ProductsArrayType }) => {
   const FeaturedProducts = products.filter((product) => product.isFeatured);
 
   return (
-    <div className="flex">
+    <div className="flex justify-center">
       <ProductsGrid products={FeaturedProducts} />
     </div>
   );
